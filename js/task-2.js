d4b741602573c88d5a6a69302d1ce661e1af5624
Denys Stepanenko <denys.workspace@gmail.com>
2025-04-07 02:54:54 -0600

    Update homework assignment titles and implement user name retrieval function

--- js/task-2.js
+++ js/task-2.js
@@ -1,44 +1,41 @@
-'use strict';
-function calcAverageCalories(days) {
-    // Якщо масив порожній, повертаємо 0
-    if (days.length === 0) {
-        return 0;
-    }
-    
-    // Обчислюємо загальну кількість калорій за допомогою reduce
-    const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
-    
-    // Обчислюємо середнє, ділячи загальну суму на кількість днів
-    const averageCalories = totalCalories / days.length;
-    
-    return averageCalories;
-}
+const getUsersWithFriend = (users, friendName) => {
+  return users.filter(user => {
+    return user.friends.includes(friendName);
+  });
+};
 
-// Код для перевірки
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
+// const allUsers = [
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"]
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"]
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ];
 
-console.log(
-  calcAverageCalories([])
-); // 0
+console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+onsole.log(getUsersWithFriend(allUsers, "Adrian Cross"));
